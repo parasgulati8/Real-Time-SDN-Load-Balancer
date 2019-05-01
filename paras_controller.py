ef1e441e4e8b49e364cff79e21a273fe25c85bf1
Paras Gulati <parasgulati8@gmail.com>
2019-05-01 19:18:45 -0400

    Monitoring working for ports
    Monitoring for port is displaying on screen but not going to file

---
+++ paras_controller.py
@@ -0,0 +1,293 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ether
+from ryu.ofproto import inet
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import arp
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import tcp
+from ryu.lib.packet import udp
+
+from operator import attrgetter
+from ryu.lib import hub
+# you may import more libs here, but the above libs should be enough
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+   
+    """
+        Constructor:
+        You can define some globally used variables inside the class
+    """
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        # arp table: for searching
+        self.arp_table={}
+        self.arp_table["10.0.0.1"] = "00:00:00:00:00:01"
+        self.arp_table["10.0.0.2"] = "00:00:00:00:00:02"
+        self.arp_table["10.0.0.3"] = "00:00:00:00:00:03"
+        self.datapaths = {}
+        self.monitor_thread = hub.spawn(self._monitor)
+        
+        self.r_s1p1 = 0
+        self.r_s1p2 = 0
+        self.r_s1p3 = 0
+        self.r_s2p1 = 0
+        self.r_s2p2 = 0
+        self.r_s2p3 = 0
+        self.r_s3p1 = 0
+        self.r_s3p2 = 0
+        self.r_s3p3 = 0
+        self.r_s4p1 = 0
+        self.r_s4p2 = 0
+        self.r_s4p3 = 0
+        self.r_s5p1 = 0
+        self.r_s5p2 = 0
+        self.r_s5p3 = 0
+    """
+        Hand-shake event call back method
+        This is the very initial method where the switch hand shake with the controller
+        It checks whether both are using the same protocol version: OpenFlow 1.3 in this case
+        Therefore in this method, you can setup some static rules.
+        e.g. the rules which sends unknown packets to the controller 
+             the rules directing TCP/UDP/ICMP traffic
+             ACL rules
+    """
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        
+        # Insert Static rule
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+        dpid = datapath.id  # classifying the switch ID
+        #Using static flows to define paths for ICMP and UDP packets
+        if dpid == 1: # switch S1
+            #10.0.0.1 is dst ip address, 1 is switch port number
+            # for switch 1, what port number should choose to forward that dst address
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.1', 10, 1)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.2', 10, 2)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.3', 10, 3)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.1', 10, 1)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.2', 10, 2)
+            
+        elif dpid == 2:
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.1', 10, 1)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.2', 10, 2)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.3', 10, 3)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.1', 10, 1)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.2', 10, 2)
+            
+        elif dpid == 3:
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.1', 10, 1)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.2', 10, 2)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.3', 10, 2)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.1', 10, 1)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.2', 10, 2)
+
+        elif dpid == 4:
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.1', 10, 2)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.2', 10, 1)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.3', 10, 3)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.1', 10, 1)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.2', 10, 2)
+            
+        elif dpid == 5:
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.1', 10, 2)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.2', 10, 2)
+            self.add_layer4_rules(datapath, inet.IPPROTO_ICMP, '10.0.0.3', 10, 1)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.1', 10, 2)
+            self.add_layer4_rules(datapath, inet.IPPROTO_UDP, '10.0.0.2', 10, 1)
+
+         
+    """ 
+        Call back method for PacketIn Message
+        This is the call back method when a PacketIn Msg is sent
+        from a switch to the controller
+        It handles L3 classification in this function:
+    """ 
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        
+        in_port = msg.match['in_port']
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+        ethertype = eth.ethertype
+
+        # process ARP 
+        if ethertype == ether.ETH_TYPE_ARP:
+            self.handle_arp(datapath, in_port, pkt)
+            return
+
+        # process IP
+        if ethertype == ether.ETH_TYPE_IP:
+            self.handle_ip(datapath, in_port, pkt)
+            return
+
+    # Member methods you can call to install TCP/UDP/ICMP fwding rules
+    def add_layer4_rules(self, datapath, ip_proto, ipv4_dst = None, priority = 1, fwd_port = None):
+        parser = datapath.ofproto_parser
+        actions = [parser.OFPActionOutput(fwd_port)]
+        match = parser.OFPMatch(eth_type = ether.ETH_TYPE_IP,
+                                ip_proto = ip_proto,
+                                ipv4_dst = ipv4_dst)
+        self.add_flow(datapath, priority, match, actions)
+
+    # Member methods you can call to install general rules
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    """
+        Methods to handle ARP. In this implementation the controller
+        generates the ARP reply msg back to the host who initiate it.
+        So the controller should parse out the ARP request;
+        Search the ARP table for correponding dst MAC;
+        Generate ARP reply;
+        And finally use PacketOut Message to send back the ARP reply
+    """
+    def handle_arp(self, datapath, in_port, pkt):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # parse out the ethernet and arp packet
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+        arp_pkt = pkt.get_protocol(arp.arp)
+        # obtain the MAC of dst IP  
+        arp_resolv_mac = self.arp_table[arp_pkt.dst_ip]
+
+        ### generate the ARP reply msg, please refer RYU documentation
+        ### the packet library section
+
+        ether_hd = ethernet.ethernet(dst = eth_pkt.src, 
+                                src = arp_resolv_mac, 
+                                ethertype = ether.ETH_TYPE_ARP);
+        arp_hd = arp.arp(hwtype=1, proto = 2048, hlen = 6, plen = 4,
+                         opcode = 2, src_mac = arp_resolv_mac, 
+                         src_ip = arp_pkt.dst_ip, dst_mac = eth_pkt.src,
+                         dst_ip = arp_pkt.src_ip);
+        arp_reply = packet.Packet()
+        arp_reply.add_protocol(ether_hd)
+        arp_reply.add_protocol(arp_hd)
+        arp_reply.serialize()
+        
+        # send the Packet Out mst to back to the host who is initilaizing the ARP
+        actions = [parser.OFPActionOutput(in_port)];
+        out = parser.OFPPacketOut(datapath, ofproto.OFP_NO_BUFFER, 
+                                  ofproto.OFPP_CONTROLLER, actions,
+                                  arp_reply.data)
+        datapath.send_msg(out)
+
+    """
+        Methods to handle TCP/IP. In this implementation the controller
+        generate the TCP RST for connections between h1 and h3.
+        In switch_features_handler() you should put a static rule to fwd 
+        those packets to the controller, and in handle_ip() you need to 
+        generate and return the TCP RST with PacketOut Message
+    """
+
+    def handle_ip(self, datapath, in_port, pkt):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+        ipv4_pkt = pkt.get_protocol(ipv4.ipv4) # parse out the IPv4 pkt
+        tcp_pkt = pkt.get_protocol(tcp.tcp)
+        
+    @set_ev_cls(ofp_event.EventOFPStateChange,
+                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def _state_change_handler(self, ev):
+        datapath = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            if datapath.id not in self.datapaths:
+                self.logger.debug('register datapath: %016x', datapath.id)
+                self.datapaths[datapath.id] = datapath
+        elif ev.state == DEAD_DISPATCHER:
+            if datapath.id in self.datapaths:
+                self.logger.debug('unregister datapath: %016x', datapath.id)
+                del self.datapaths[datapath.id]
+                
+    def _monitor(self):
+        while True:
+            for dp in self.datapaths.values():
+                self._request_stats(dp)
+            hub.sleep(10)
+            
+    def _request_stats(self, datapath):
+        self.logger.debug('send stats request: %016x', datapath.id)
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        req = parser.OFPFlowStatsRequest(datapath)
+        datapath.send_msg(req)
+
+        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def _flow_stats_reply_handler(self, ev):
+        body = ev.msg.body
+
+        self.logger.info('datapath         '
+                         'in-port  eth-dst           '
+                         'out-port packets  bytes')
+        self.logger.info('---------------- '
+                         '-------- ----------------- '
+                         '-------- -------- --------')
+        for stat in sorted([flow for flow in body if flow.priority == 1],
+                           key=lambda flow: (flow.match['in_port'],
+                                             flow.match['eth_dst'])):
+            self.logger.info('%016x %8x %17s %8x %8d %8d',
+                             ev.msg.datapath.id,
+                             stat.match['in_port'], stat.match['eth_dst'],
+                             stat.instructions[0].actions[0].port,
+                             stat.packet_count, stat.byte_count)
+            
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def _port_stats_reply_handler(self, ev):
+        body = ev.msg.body
+
+        self.logger.info('datapath         port     '
+                         'rx-pkts  rx-bytes rx-error '
+                         'tx-pkts  tx-bytes tx-error')
+        self.logger.info('---------------- -------- '
+                         '-------- -------- -------- '
+                         '-------- -------- --------')
+        for stat in sorted(body, key=attrgetter('port_no')):
+            self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
+                             ev.msg.datapath.id, stat.port_no,
+                             stat.rx_packets, stat.rx_bytes, stat.rx_errors,
+                             stat.tx_packets, stat.tx_bytes, stat.tx_errors)
+            
+            f = open("demofile.txt", "a+")
+            f.write('datapath         port     '
+                         'rx-pkts  rx-bytes rx-error '
+                         'tx-pkts  tx-bytes tx-error')
+            f.write('---------------- -------- '
+                         '-------- -------- -------- '
+                         '-------- -------- --------')
+            f.write('%016x %8x %8d %8d %8d %8d %8d %8d'
+                             %(ev.msg.datapath.id, stat.port_no,
+                             stat.rx_packets, stat.rx_bytes, stat.rx_errors,
+                             stat.tx_packets, stat.tx_bytes, stat.tx_errors))
+            f.close()
+            
